2d6e241097f590cc27eafcdfa0a56337752f1e60
zengrui <772701414@qq.com>
2021-06-23 14:11:15 +0800

    38

--- src/main/java/M剑指office38字符串的排序.java
+++ src/main/java/M剑指office38字符串的排序.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @Author: 曾睿
@@ -7,39 +6,90 @@ import java.util.List;
  */
 public class M剑指office38字符串的排序 {
     public static void main(String[] args) {
-        System.out.println(permutation("abc"));
+        for (Object o : permutation("123")) {
+            System.out.println(o);
+        }
+//        Arrays.stream(permutation("123")).forEach(System.out::println);
+
     }
-
-
-
 
     /**
      * 取值，第一次去第一个值，然后把改值去掉
      * @param s
      * @return
      */
-    public static String[] permutation(String s) {
-        List<StringBuilder> list = new ArrayList<>();
-        StringBuilder stringBuilder = new StringBuilder(s);
+    public static Object[] permutation(String s) {
+        List<Map<Integer,Character>> list = new ArrayList<>();
         // 第一个字母按照顺序，剩下的就从头开始取
         for (int m = 0; m < s.length(); m++) {
-            // 第一个字母的
-            StringBuilder newStr = new StringBuilder();
-            char c = stringBuilder.charAt(m);
-            stringBuilder.deleteCharAt(0);
-            newStr.append(c);
-            // 剩下字母一直从第一个开始取
-            while (stringBuilder.charAt(0) == 32){
-                for (int n = 0; n < s.length(); n++) {
-                    c = stringBuilder.charAt(n);
+            // 把第m个字母放在第一位
+            for (int i = 0; i < s.length(); i++) {
+                StringBuilder stringBuilder = new StringBuilder(s);
+                Map<Integer,Character> newStr = new HashMap<>(s.length());
+                newStr.put(i, stringBuilder.charAt(m));
+                stringBuilder.deleteCharAt(m);
+                // 剩下字母一直从第一个开始取
+                for (int j = 0; j < s.length()-1; j++) {
+                    if(j >= i){
+                        newStr.put(j+1,stringBuilder.charAt(0));
+                    }else {
+                        newStr.put(j,stringBuilder.charAt(0));
+                    }
                     stringBuilder.deleteCharAt(0);
-                    newStr.append(c);
                 }
+                list.add(newStr);
             }
-        list.add(newStr);
         }
-        return list.toArray(new String[list.size()-1]);
+        List<StringBuilder> ss = new ArrayList<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        list.forEach(l->{
+            l.keySet().forEach(
+                    c ->{
+                        stringBuilder.append(l.get(c));
+                        ss.add(stringBuilder);
+                    });
+
+        });
+        Object[] as = ss.stream().distinct().toArray();
+        return as;
     }
+
+
+
+//    /**
+//     * 取值，第一次去第一个值，然后把改值去掉
+//     * @param s
+//     * @return
+//     */
+//    public static StringBuilder[] permutation(String s) {
+//        int len = 1;
+//        for (int i = s.length(); i > 0; i--) {
+//            len *= i;
+//        }
+//        List<StringBuilder> list = new ArrayList<>();
+//        // 第一个字母按照顺序，剩下的就从头开始取
+//        for (int m = 0; m < s.length(); m++) {
+//            StringBuilder stringBuilder = new StringBuilder(s);
+//            // 第一个字母的
+//            StringBuilder newStr = new StringBuilder();
+////            for (int i = 0; i <len; i++) {
+//                newStr.append(stringBuilder.charAt(m));
+//                stringBuilder.deleteCharAt(m);
+//                // 剩下字母一直从第一个开始取
+//                while (!"".equals(stringBuilder.toString())){
+//                    newStr.append(stringBuilder.charAt(0));
+//                    stringBuilder.deleteCharAt(0);
+////                for (int n = 0; n < s.length(); n++) {
+////                    char c = stringBuilder.charAt(n);
+////                    newStr.append(c);
+////                    stringBuilder.deleteCharAt(0);
+////                }
+////                }
+//                list.add(newStr);
+//            }
+//        }
+//        return list.toArray(new StringBuilder[0]);
+//    }
 
 
 
