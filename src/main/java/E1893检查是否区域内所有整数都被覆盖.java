4ed67da548b87ab42b38fbb7232e7f6c01598980
zengrui <772701414@qq.com>
2021-07-23 17:02:10 +0800

    1893

---
+++ src/main/java/E1893检查是否区域内所有整数都被覆盖.java
@@ -0,0 +1,35 @@
+/**
+ * @Author: 曾睿
+ * @Date: 2021/7/23 10:11
+ */
+public class E1893检查是否区域内所有整数都被覆盖 {
+
+
+    public static void main(String[] args) {
+        int[][] arr = new int[][]{{1, 50}};
+
+        System.out.println(isCovered(arr,1,50));
+
+    }
+
+
+    public static boolean isCovered(int[][] ranges, int left, int right) {
+        // 合并数组
+        int[] arr = new int[51];
+        for (int[] range : ranges) {
+            for (int i = range[0]; i <= range[1]; i++) {
+                // 合并数组，合并后的结果+1
+                arr[i] += 1;
+            }
+        }
+        for (int i = left; i <= right; i++) {
+            arr[i] -= 1;
+            if(arr[i] < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
