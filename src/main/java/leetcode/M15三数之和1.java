2531460f842140a4d6c70a02284cff8e2497b24d
Jerry <Jerry.Zeng@cdpgroupltd.com>
2022-08-16 18:40:12 +0800

    变更

--- src/main/java/leetcode/M15三数之和1.java
+++ src/main/java/leetcode/M15三数之和1.java
@@ -11,63 +11,58 @@ import java.util.List;
 public class M15三数之和1 {
 
     public static void main(String[] args) {
-//        List<List<Integer>> lists = threeSum(new int[]{0,0,0,0});
-        List<List<Integer>> lists = threeSum(new int[]{-1,0,1,2,-1,-4});
+        // list<list<Integer>> lists = threeSum(new int[]{0,0,0,0});
+        List<List<Integer>> lists = threeSum(new int[]{-1, 0, 1, 2, -1, -4});
         System.out.println(lists);
 
     }
 
     public static List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
-        if (nums.length < 3) {
-            return new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        if (nums == null || nums.length < 3){
+            return ans;
         }
-        int i = 1;
-        int j = nums.length - 1;
-        for (int k = 0; k < nums.length-2 && nums[k] <= 0; ) {
-            while(true){
-                int sum = nums[k] + nums[i] + nums[j];
-                if (sum < 0) {
-                    // exit
-                    if (i == j-1) {
-                        i = k + 2;
-                        break;
-                    } else {
-                        int t = i;
-                        do {
-                            i++;
-                        } while (nums[i] == nums[t] && i < j-1);
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            // i退出
+            if (nums[i] > 0){
+                break;
+            }
+            // i去重
+            if (i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+
+            // 开始计算r、l
+            int l = i + 1;
+            int r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    while (l < r && nums[l] == nums[l + 1]){
+                        l++;
                     }
-                } else if (sum > 0) {
-                    // exit
-                    if (i == j-1) {
-                        j = nums.length -1;
-                        break;
-                    } else {
-                        int t = j;
-                        do {
-                            j--;
-                        } while (nums[j] == nums[t] && i < j-1);
+                    l++;
+                    while (l < r && nums[r] == nums[r - 1]){
+                        r--;
                     }
-                } else {
-                    // == 0
-                    List<Integer> list = new ArrayList<>();
-                    list.add(nums[k]);
-                    list.add(nums[i]);
-                    list.add(nums[j]);
-                    res.add(list);
-                    j = nums.length -1;
-                    i = k + 2;
-                    break;
+                    r--;
+                } else if (sum < 0){
+                    while (l < r && nums[l] == nums[l + 1]){
+                        l++;
+                    }
+                    l++;
+                }
+                else if (sum > 0){
+                    while (l < r && nums[r] == nums[r - 1]){
+                        r--;
+                    }
+                    r--;
                 }
             }
-            int t = k;
-            do {
-                k++;
-            } while (nums[k] == nums[t] && k < j-2);
         }
-        return res;
+        return ans;
     }
 
 }
